c0155ad3eede56fc90edd639d8eaf332a34df103
Breno Lessa <breno.lessa.andrade@gmail.com>
2015-11-17 17:58:04 -0300

    implementando admin

--- admin.php
+++ admin.php
@@ -2,14 +2,63 @@
 	session_start();
 	require 'app/Config.inc.php';
 ?>
-
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="ISO-8859-1">
-	<title></title>
-</head>
-<body>
+<html lang="pt-br">
+  <head>
+    <meta charset="ISO-8859-1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title>Repositório Institucional - IFBA - VCA</title>
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/repositorio.css" rel="stylesheet">
+
+    <script src="assets/js/jquery-2.1.4.min.js"></script>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+  	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" target="__blank" href="http://conquista.ifba.edu.br/">
+	      	<img alt="Repositório Institucional IFBA Campus Vitória da Conquista" class="img-responsive img" src="assets/images/ifba.png">
+	      </a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li class="li-login"><a href="index.php" class="login" style="color:#1de571;" title="Página Inicial"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> Home</a></li>
+	        <!-- <li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ADMIN <span class="caret"></span></a>
+	          <ul class="dropdown-menu">
+	            <li><a href="#">Action</a></li>
+	            <li><a href="#">Another action</a></li>
+	            <li><a href="#">Something else here</a></li>
+	            <li role="separator" class="divider"></li>
+	            <li><a href="#">Separated link</a></li>
+	          </ul>
+	        </li> -->
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+  	
+
 	<?php
 			
 		$login = new Login();
@@ -44,20 +93,45 @@
 		
 		
 	?>
-	<h1>Administrar Site:</h1>
-	<form action="" name="AdminLoginForm" method="post">
-		<label>
-			<span>E-mail:</span>
-			<input type="text" name="user" />
-		</label>
-		
-		<label>
-			<span>Senha:</span>
-			<input type="password" name="pass" />
-		</label>
-		
-		<input type="submit" name="AdminLogin" value="Logar" />
-		
-	</form>
-</body>
+
+
+
+
+	<div class="container abc">
+
+		<div class="row text-center">
+			<!-- <h1>RI<span class="ft">IFBA</span></h1> -->
+			<h2>ÁREA DO ADMINISTRADOR</h2>
+			
+
+			<div class="col-md-12 col-lg-12">
+				<form action="" method="post" name="AdminLoginForm">
+
+					<div class="form-group col-lg-12 col-md-12">
+				    	<label class="sr-only" for="">Usuário</label>
+						<input type="text" class="form-control inp" name="user" placeholder="Usuário" required>
+					</div>
+
+					<div class="form-group col-lg-12 col-md-12">
+				    	<label class="sr-only" for="">Usuário</label>
+						<input type="password" class="form-control inp" name="pass" placeholder="Senha" required>
+					</div>
+
+					
+
+					<div class="form-group col-lg-2 col-md-2">
+						<input type="submit" class="btn btn-default inp bt-lg" name="AdminLogin" value="Logar">
+					</div>
+
+				</form>
+
+			</div>
+		</div>
+
+	</div>
+
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
+  </body>
 </html>
